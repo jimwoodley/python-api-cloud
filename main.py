f696b518c0c1c7c3ad8633dd1c80e4fc24f78fc8
jimwoodley <linleyop2@gmail.com>
2025-08-13 09:18:43 +0100

    added basic functionality to main.py, more complex data to be added later

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import fastapi from FastAPI
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
 
 @app.get('/hello')
 def read_hello():
     return {"hello": "Hello, World!"}
+
+users = [
+    User(id=1, name='James', email='james@example.com'),
+    User(id=2, name='Naomi', email='naomi@example.com')
+]
